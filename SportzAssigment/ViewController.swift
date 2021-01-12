52285760b2d4bf322f0d10905a66addfb5bbfe15
Dinesh Tanwar <dtanwar@e-emphasys.com>
2021-01-12 18:02:44 +0530

    Initial Commit

---
+++ SportzAssigment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportzAssigment
+//
+//  Created by Dinesh Tanwar on 12/01/21.
+//  Copyright © 2021 Dinesh Tanwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
